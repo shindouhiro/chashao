fcd70c9ffc2ad8fad75c429fbcf33f99738e6b76
shindouhiro <shindouhiro@gmail.com>
2019-03-25 14:19:00 +0800

    Init

--- src/containers/postShow.js
+++ src/containers/postShow.js
@@ -3,6 +3,8 @@ import { withStyles, createStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Img from '../images/20190314115321A5j1BfL2PxN9eDcz.jpeg';
 import Tag from '../components/Tag/tag';
+import renderHTML from 'react-render-html';
+import { RootNode } from '../gunDB';
 const styles = () => createStyles({
    wrap: {
      width: '96%',
@@ -39,49 +41,72 @@ const styles = () => createStyles({
     color: '#363636',
    },
 })
-const PostShow = (props) => {
-    const { classes } = props;
-    console.log(props)
-    return(
-        <div className={classes.wrap}>
-            <div className={classes.title}>
-            休·杰克曼 2020 年重回百老汇，出演音乐剧《乐器推销员》
-            </div>
-            <div className={classes.ImgWrap}>
-                <img src={Img} className={classes.img} />
-            </div>
-            <div className={classes.content}>
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            尽管休·杰克曼最广为人知的是《X 战警》中的“金刚狼”一角，但在那之前他的演艺事业是从澳大利亚的剧院舞台上开始的，而且是音乐剧
-            </div>
-            <Tag />
-        </div>
-    )
+class PostShow extends React.Component{
+    constructor(props) {
+        super(props);
+        RootNode.get('status').put("online");
+        this.state = {
+            post: {},
+            loading: true,
+            loadingVisited: true,
+        }
+    }
+    componentDidMount(){
+    
+        const { match } = this.props;
+        RootNode.get("posts").map((post)=>(post && post.id===match.params.id)?post: undefined)
+        .once((data, key)=>{
+            const { post, loading} = this.state;
+            if(loading ===false && post.id){
+                return false;
+            }
+            document.title =  data.title;
+            let updateCount = data.visited;
+            if(!updateCount){
+                updateCount=0
+            }
+            this.setState({
+                post: data,
+                loading: false,
+            })
+            RootNode.get("posts").get(key).get("visited").put(updateCount+1, (ack)=>{
+                this.setState({
+                    loadingVisited: false,
+                })
+               
+            });
+
+        })
+        
+       
+        
+    }
+    render(){
+        const { classes } = this.props;
+        const { post, loading, loadingVisited} = this.state;
+        document.body.scrollTop = document.documentElement.scrollTop = 154;
+        return(
+                <div className={classes.wrap}>
+                    <div className={classes.title}>
+                    {post.title}
+                    </div>
+                    <div className={classes.ImgWrap}>
+                        <img src={post.coverUrl} className={classes.img} />
+                    </div>
+                    <div className={classes.content}>
+                    {   
+                                    post.body ? 
+                                    renderHTML(post.body) 
+                                    : 
+                                    renderHTML("<span></span>")
+                                
+                      }
+                    </div>
+                    <Tag />
+                </div>
+        )
+    }
+   
 }
 
 
